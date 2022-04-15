ced3c11bc80d03116e7a5ac00eec2792c7eba48e
Robin Lovelace <rob00x@gmail.com>
2022-04-15 15:35:38 +0100

    Document
    Former-commit-id: 6e1bd768d8502a40ef4a6e8d9ed1e2affd01f447

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Example zones and centroids
+#'
+#' `zones` and `centroids` represent administrative zones between which
+#' flows are to be estimated.
+#' @note The schema data can be (re-)generated using code in `data-raw`
+#' @docType data
+#' @keywords datasets
+#' @name zones
+#' @aliases centroids
+NULL
+
+#' Example OD dataset
+#'
+#' Example OD dataset from the 2011 UK Census
+#' @note Regenerate the data with scripts in the `data-raw` directory.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name od_census_min
+NULL
