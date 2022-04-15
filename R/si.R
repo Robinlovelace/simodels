04fc02a7f8c9f8d8bf82f4772b1ae1ff1a632c20
Robin Lovelace <rob00x@gmail.com>
2022-04-15 12:18:01 +0100

    Update example data

--- R/si.R
+++ R/si.R
@@ -1,3 +1,8 @@
-si = function(origins) {
-    
+#' Spatial Interactions
+#''
+#' Estimate movement between origins and destinations with a spatial interaction model
+#' @examples
+#' origins = zones
+si = function(origins, destinations, max_euclidean_distance = 20, fun = lm) {
+
 }
