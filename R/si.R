e948ab1ef1b8c24b10ebb5a7331ad9397e81a78b
Jakub Nowosad <tupiszakaczki@gmail.com>
2022-04-15 16:49:23 +0200

    fixes two notes

--- R/si.R
+++ R/si.R
@@ -72,23 +72,26 @@ si_to_od = function(origins, destinations, max_euclidean_distance = 10000) {
 #'   such that the total for each zone of origin cannot go above this value.
 #' @param ... Arguments passed to `fun`
 #' @export
+#' @importFrom rlang .data
 #' @examples
 #' od = si_to_od(zones, zones)
 #' fun_dd = function(od, d = "distance_euclidean", beta = 0.3) exp(-beta * od[[d]] / 1000)
 #' od_dd = si_model(od, fun = fun_dd)
 #' plot(od$distance_euclidean, od_dd$res)
 #' fun = function(od, O, n, d, beta) od[[O]] * od[[n]] * exp(-beta * od[[d]]/1000)
-#' od_output = si_model(od, fun = fun, beta = 0.3, O = "origin_all", n = "destination_all", d = "distance_euclidean")
+#' od_output = si_model(od, fun = fun, beta = 0.3, O = "origin_all", 
+#'   n = "destination_all", d = "distance_euclidean")
 #' head(od_output)
 #' plot(od$distance_euclidean, od_output$res)
-#' od_pconst = si_model(od, fun = fun, beta = 0.3, O = "origin_all", n = "destination_all", d = "distance_euclidean", var_p = origin_all)
+#' od_pconst = si_model(od, fun = fun, beta = 0.3, O = "origin_all", n = "destination_all",
+#'   d = "distance_euclidean", var_p = origin_all)
 #' plot(od_pconst$distance_euclidean, od_pconst$res_constrained)
 #' plot(od_pconst["res"], logz = TRUE)
 si_model = function(od, fun, var_p, ...) {
     od$res = fun(od, ...)
     if (!missing(var_p)) {
-        od_grouped = dplyr::group_by(od, O)
-        od_grouped = dplyr::mutate(od_grouped, res = res / sum(res) * mean( {{var_p}} ))
+        od_grouped = dplyr::group_by(od, .data$O)
+        od_grouped = dplyr::mutate(od_grouped, res = .data$res / sum(.data$res) * mean( {{var_p}} ))
         od = dplyr::ungroup(od_grouped)
     }
     od
