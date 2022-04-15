0d3c44386ae17d7b7003b3e12b39ec15f5e96e8b
Robin Lovelace <rob00x@gmail.com>
2022-04-15 15:13:11 +0100

    si_model mvp

--- R/si.R
+++ R/si.R
@@ -1,6 +1,7 @@
-#' Spatial Interaction Model
+#' Prepare OD
 #'
-#' Estimate movement between origins and destinations 
+#' Prepares an OD data frame that next could be used to
+#' estimate movement between origins and destinations 
 #' with a spatial interaction model
 #'
 #' @param origins `sf` object representing origin locations/zones
@@ -40,12 +41,12 @@ si_to_od = function(origins, destinations, max_euclidean_distance = 10000) {
   origins_to_join = sf::st_drop_geometry(origins)
   names(origins_to_join) = paste0("origin_", names(origins_to_join))
   names(origins_to_join)[1] = names(od_df)[1]
-  od_dfj = dplyr::inner_join(od_df, origins_to_join)
+  od_dfj = dplyr::inner_join(od_df, origins_to_join, by = "O")
   # join destination attributes
   destinations_to_join = sf::st_drop_geometry(destinations)
   names(destinations_to_join) = paste0("destination_", names(destinations_to_join)) # nolint
   names(destinations_to_join)[1] = names(od_df)[2]
-  od_dfj = dplyr::inner_join(od_dfj, destinations_to_join)
+  od_dfj = dplyr::inner_join(od_dfj, destinations_to_join, by = "D")
   names(od_dfj)
 
   # convert to sfc:
@@ -56,3 +57,39 @@ si_to_od = function(origins, destinations, max_euclidean_distance = 10000) {
   sf::st_sf(od_dfj, geometry = od_sfc)
 }
 
+#' Run a spatial interaction model
+#'
+#' Executes a spatial iteraction model based on an OD data frame 
+#' and user-specified function
+#' 
+#' @param od A data frame representing origin-destination data, e.g. as created by
+#'   [si_to_od()]
+#' @param fun A function that calculates the interaction (e.g. the number of trips)
+#'   between each OD pair
+#' @param var_p Use this argument to run a 'production constrained' SIM.
+#'   Character string corresponding to column in the `od`
+#'   dataset that constrains the total 'interaction' (e.g. n. trips) for all OD pairs
+#'   such that the total for each zone of origin cannot go above this value.
+#' @param ... Arguments passed to `fun`
+#' @export
+#' @examples
+#' od = si_to_od(zones, zones)
+#' fun_dd = function(od, d = "distance_euclidean", beta = 0.3) exp(-beta * od[[d]] / 1000)
+#' od_dd = si_model(od, fun = fun_dd)
+#' plot(od$distance_euclidean, od_dd$res)
+#' fun = function(od, O, n, d, beta) od[[O]] * od[[n]] * exp(-beta * od[[d]]/1000)
+#' od_output = si_model(od, fun = fun, beta = 0.3, O = "origin_all", n = "destination_all", d = "distance_euclidean")
+#' head(od_output)
+#' plot(od$distance_euclidean, od_output$res)
+#' od_pconst = si_model(od, fun = fun, beta = 0.3, O = "origin_all", n = "destination_all", d = "distance_euclidean", var_p = origin_all)
+#' plot(od_pconst$distance_euclidean, od_pconst$res_constrained)
+#' plot(od_pconst["res_constrained"], logz = TRUE)
+si_model = function(od, fun, var_p, ...) {
+    od$res = fun(od, ...)
+    if (!missing(var_p)) {
+        od_grouped = dplyr::group_by(od, O)
+        od_grouped = dplyr::mutate(od_grouped, res = res / sum(res) * mean( {{var_p}} ))
+        od = dplyr::ungroup(od_grouped)
+    }
+    od
+}
