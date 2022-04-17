acfcbe51da7bd40aaaafb319e09aa280e76efcea
Robin Lovelace <rob00x@gmail.com>
2022-04-17 08:13:00 +0100

    Destination examples, progress on #3

--- R/si.R
+++ R/si.R
@@ -11,15 +11,28 @@
 #'   and therefore included in the SIM.
 #' @export
 #' @examples
-#' origins = si_centroids
-#' destinations = si_centroids
-#' odsf = si_to_od(origins, destinations, max_dist = 2000)
+#' library(sf)
+#' origins = si_centroids[c(1, 2, 99), ]
+#' destinations = origins
+#' plot(origins$geometry)
+#' odsf = si_to_od(origins, destinations, max_dist = 1200)
+#' plot(odsf)
+#' # note: result contains intrazonal flows represented by linestrings
+#' # with a length of 0, e.g.:
+#' sf::st_coordinates(odsf$geometry[1])
+#' # With different destinations compared with origins
+#' library(sf)
+#' origins = si_centroids[c(2, 99), c(1, 6, 7)]
+#' destinations = si_centroids[1, c(1, 6, 8)]
+#' odsf = si_to_od(origins, destinations)
+#' nrow(odsf) # no intrazonal flows
 #' plot(odsf)
 si_to_od = function(origins, destinations, max_dist = Inf) {
-  if(!identical(origins, destinations)) {
-        stop("Origins and destinations are different, not implemented yet")
-    }
-  od_df = od::points_to_od(origins)
+  if(identical(origins, destinations)) {
+    od_df = od::points_to_od(origins)
+  } else {
+    od_df = od::points_to_od(origins, destinations)
+  }
   od_df$distance_euclidean = geodist::geodist(
       x = od_df[c("ox", "oy")],
       y = od_df[c("dx", "dy")],
@@ -37,20 +50,21 @@ si_to_od = function(origins, destinations, max_dist = Inf) {
           pct_kept, "% of all possible OD pairs"
           )
   }
+  od_sfc = od::odc_to_sfc(od_df[3:6])
+  sf::st_crs(od_sfc) = 4326 # todo: add CRS argument?
+  od_df = od_df[-c(3:6)]
+  
   # join origin attributes
   origins_to_join = sf::st_drop_geometry(origins)
   names(origins_to_join) = paste0("origin_", names(origins_to_join))
   names(origins_to_join)[1] = names(od_df)[1]
-  od_sfc = od::odc_to_sfc(od_df[3:6])
-  sf::st_crs(od_sfc) = 4326 # todo: add CRS argument?
-  od_df = od_df[-c(3:6)]
   od_dfj = dplyr::inner_join(od_df, origins_to_join, by = "O")
   # join destination attributes
   destinations_to_join = sf::st_drop_geometry(destinations)
   names(destinations_to_join) = paste0("destination_", names(destinations_to_join)) # nolint
   names(destinations_to_join)[1] = names(od_df)[2]
   od_dfj = dplyr::inner_join(od_dfj, destinations_to_join, by = "D")
-  names(od_dfj)
+  # names(od_dfj)
   # create and return sf object
   sf::st_sf(od_dfj, geometry = od_sfc)
 }
