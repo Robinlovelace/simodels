68bf56d2039a28ccdd978670f21764ca145dd7a4
Robin Lovelace <rob00x@gmail.com>
2022-04-21 22:54:44 +0100

    Change output variable name for #9

--- R/si_model.R
+++ R/si_model.R
@@ -18,21 +18,25 @@
 #' od = si_to_od(si_zones, si_zones, max_dist = 4000)
 #' fun_dd = function(od, d = "distance_euclidean", beta = 0.3) exp(-beta * od[[d]] / 1000)
 #' od_dd = si_calculate(od, fun = fun_dd)
-#' plot(od$distance_euclidean, od_dd$res)
+#' plot(od$distance_euclidean, od_dd$interaction)
 #' fun = function(od, O, n, d, beta) od[[O]] * od[[n]] * exp(-beta * od[[d]]/1000)
 #' od_output = si_calculate(od, fun = fun, beta = 0.3, O = "origin_all", 
 #'   n = "destination_all", d = "distance_euclidean")
 #' head(od_output)
-#' plot(od$distance_euclidean, od_output$res)
+#' plot(od$distance_euclidean, od_output$interaction)
 #' od_pconst = si_calculate(od, fun = fun, beta = 0.3, O = "origin_all", n = "destination_all",
 #'   d = "distance_euclidean", constraint_p = origin_all)
-#' plot(od_pconst$distance_euclidean, od_pconst$res)
-#' plot(od_pconst["res"], logz = TRUE)
+#' plot(od_pconst$distance_euclidean, od_pconst$interaction)
+#' plot(od_pconst["interaction"], logz = TRUE)
 si_calculate = function(od, fun, constraint_p, ...) {
-  od$res = fun(od, ...)
+  od$interaction = fun(od, ...)
   if (!missing(constraint_p)) {
     od_grouped = dplyr::group_by(od, .data$O)
-    od_grouped = dplyr::mutate(od_grouped, res = .data$res / sum(.data$res) * mean( {{constraint_p}} ))
+    od_grouped = dplyr::mutate(
+      od_grouped,
+      interaction = .data$interaction /
+        sum(.data$interaction) * mean( {{constraint_p}} )
+      )
     od = dplyr::ungroup(od_grouped)
   }
   od
@@ -48,11 +52,13 @@ si_calculate = function(od, fun, constraint_p, ...) {
 #' m = lm(od$origin_all ~ od$origin_bicycle)
 #' od_updated = si_predict(od, m)
 si_predict = function(od, model, constraint_p) {
-  od$res = stats::predict(model, od)
+  od$interaction = stats::predict(model, od)
   if (!missing(constraint_p)) {
     od_grouped = dplyr::group_by(od, .data$O)
     od_grouped = dplyr::mutate(
-      od_grouped, res = .data$res / sum(.data$res) * mean( {{constraint_p}} )
+      od_grouped,
+      interaction = .data$interaction /
+        sum(.data$interaction) * mean( {{constraint_p}} )
       )
     od = dplyr::ungroup(od_grouped)
   }
