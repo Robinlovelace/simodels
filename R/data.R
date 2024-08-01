b2f259e1de067c79391c604bb8229df9eecd6be3
robinlovelace <rob00x@gmail.com>
2024-08-01 23:26:55 +0100

    Add new datasets (#34)

--- R/data.R
+++ R/data.R
@@ -60,3 +60,28 @@ NULL
 #' @examples
 #' head(od_aus)
 NULL
+
+#' Example zones dataset: administrative zones of York
+#' 
+#' See data-raw/zones_york.qmd for details on the data source.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name zones_york
+#' @examples
+#' head(zones_york)
+#' plot(zones_york$geometry)
+NULL
+
+#' Example destinations dataset: schools in York
+#' 
+#' Example dataset from York, UK
+#' 
+#' See data-raw/zones_york.qmd for details on the data source.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name destinations_york
+#' @examples
+#' head(destinations_york)
+NULL
